b740841e7cd28325dbd53494c3aa73e3fdda4304
Gabriel Henriquez Alguacil <gabha2107@gmail.com>
2019-10-04 18:49:03 -0400

    HomePage list and Course detail done.

--- NewBeeProject/ViewModels/CourseDetailPageViewModel.cs
+++ NewBeeProject/ViewModels/CourseDetailPageViewModel.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace NewBeeProject.ViewModels
 {
     public class CourseDetailPageViewModel
     {
+        INavigationService _navigationService;
 
+        public DelegateCommand MapCommand { get; set; }
+        public DelegateCommand CallCommand { get; set; }
+
+        public CourseDetailPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            MapCommand = new DelegateCommand(async() =>
+            {
+                await GoToMap();
+            });
+
+            //CallCommand = new DelegateCommand<AreaPhones>((param) =>
+            //{
+            //    Device.OpenUri(new Uri(String.Format("tel:{0}", $"{param.Phone}")));
+            //});
+        }
+        public async Task GoToMap()
+        {
+            await _navigationService.NavigateAsync($"{NavConstants.Navigation}/{NavConstants.Map}");
+        }
     }
 }
