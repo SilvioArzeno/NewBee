5f678694d90ac3ae3dd38cea43b6dd010c4b039c
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-11 14:54:25 -0400

    Filled the directory list

--- NewBeeProject/ViewModels/PhoneNumbersPageViewModel.cs
+++ NewBeeProject/ViewModels/PhoneNumbersPageViewModel.cs
@@ -1,15 +1,24 @@
-﻿using NewBeeProject.Models;
+﻿using MonkeyCache.FileStore;
+using NewBeeProject.Models;
+using NewBeeProject.Services;
 using Prism.Commands;
+using Prism.Navigation;
 using Prism.Services;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace NewBeeProject.ViewModels
 {
-    public class PhoneNumbersPageViewModel
+    public class PhoneNumbersPageViewModel : BaseViewModel
     {
+        public ObservableCollection<Directory> DirectoryList { get; set; }
         public DelegateCommand<Directory> CallCommand { get; set; }
-        public PhoneNumbersPageViewModel(IDeviceService deviceService)
+        public PhoneNumbersPageViewModel(INavigationService navigationService,IDeviceService deviceService, APIService APIservice) : base(navigationService)
         {
+
+            DirectoryList = new ObservableCollection<Directory>(Barrel.Current.Get<List<Directory>>("AllDirectoriesList"));
+
             CallCommand = new DelegateCommand<Directory>((param) =>
             {
                 deviceService.OpenUri(new Uri(uriString: $"tel:{$"{param.AreaPhoneNumber}"}"));
