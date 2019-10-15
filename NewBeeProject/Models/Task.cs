c56d2043ac6bc8df9eb54566fdc5e192a7028b46
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-14 23:23:45 -0400

    Added Task Model

---
+++ NewBeeProject/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBeeProject.Models
+{
+    public class Task
+    {
+        public string TaskName { get; set; }
+        public string TaskCourse { get; set; }
+        public DateTime TaskDueDate { get; set; }
+        public string TaskDescription { get; set; }
+    }
+}
