3c4f4de69b75e77d69876eddb6dd71a944e4b754
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-11-01 16:29:14 -0400

    Fixed Json serializables

--- NewBeeProject/Models/Student.cs
+++ NewBeeProject/Models/Student.cs
@@ -8,29 +8,22 @@ namespace NewBeeProject.Models
 {
    public class Student
        {
-        [JsonProperty(PropertyName ="active")]
         public bool Active { get; set; } = true;
 
 
-        [JsonProperty(PropertyName = "nombres")]
         public string FirstName { get; set; }
 
-        [JsonProperty(PropertyName = "apellidos")]
         public string LastName { get; set; }
 
-        [JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
 
-        [JsonProperty(PropertyName = "matricula")]
         public string StudentID { get; set; }
 
-
-        [JsonProperty(PropertyName = "password")]
         public string Password { get; set; }
 
         public List<Course> StudentCoursesList { get; set; }
 
-      
+        public List<CollegeTask> StudentTaskList { get; set; }
     }
     
 }
--- NewBeeProject/ViewModels/TaskPageViewModel.cs
+++ NewBeeProject/ViewModels/TaskPageViewModel.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using MonkeyCache.FileStore;
+using NewBeeProject.Models;
+using NewBeeProject.Services;
+using Prism.Navigation;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace NewBeeProject.ViewModels
 {
-    class TaskPageViewModel
+    class TaskPageViewModel :BaseViewModel
     {
+
+        public ObservableCollection<CollegeTask> TaskList { get; set; }
+       public CollegeTask SelectedTask { get; set; }
+
+        public Student LoggedStudent { get; set; }
+
+
+        public TaskPageViewModel(INavigationService navigationService, IAPIService APIService) : base(navigationService)
+        {
+            LoggedStudent = Barrel.Current.Get<Student>("LoggedStudent");
+            
+        }
     }
 }
